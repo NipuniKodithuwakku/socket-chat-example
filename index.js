1fadbf1a8f72199295fd26930b6ee01ad6ca2466
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-17 06:48:51 +0530

    add socket disconnect event

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require("express")();
 var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -7,6 +8,9 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 http.listen(3000, () => {
