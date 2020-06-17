064c430940e99185ab20cc9646e847cf7a8d2659
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-17 07:04:19 +0530

    print out chat message event

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  //   console.log("a user connected");
+  //   socket.on("disconnect", () => {
+  //     console.log("user disconnected");
+  //   });
+  socket.on("chat message", (msg) => {
+    console.log("message:" + msg);
   });
 });
 
