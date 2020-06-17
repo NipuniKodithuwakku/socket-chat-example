18b03661fcc60241c377f1700150158fce5bc3ce
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-17 07:43:24 +0530

    add environment variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+var port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -12,5 +13,5 @@ io.on("connection", (socket) => {
 });
 
 http.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log("listening on *:" + port);
 });
