4dc9e62fd0496ac8715782345d4560ca4fe9fe5e
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-17 07:32:19 +0530

    add gitignore file

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ var io = require("socket.io")(http);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
 io.on("connection", (socket) => {
-  //   console.log("a user connected");
-  //   socket.on("disconnect", () => {
-  //     console.log("user disconnected");
-  //   });
   socket.on("chat message", (msg) => {
-    console.log("message:" + msg);
+    console.log("message: " + msg);
   });
 });
+
+// io.on("connection", (socket) => {
+//   //   console.log("a user connected");
+//   //   socket.on("disconnect", () => {
+//   //     console.log("user disconnected");
+//   //   });
+//   socket.on("chat message", (msg) => {
+//     console.log("message:" + msg);
+//   });
+// });
 
 http.listen(3000, () => {
   console.log("listening on *:3000");
