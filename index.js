9bb07a526fb6460c5499751a8dbc0a296dec45a4
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-17 07:40:35 +0530

    send message to everyone including sender

--- index.js
+++ index.js
@@ -7,19 +7,9 @@ app.get("/", (req, res) => {
 });
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
 });
-
-// io.on("connection", (socket) => {
-//   //   console.log("a user connected");
-//   //   socket.on("disconnect", () => {
-//   //     console.log("user disconnected");
-//   //   });
-//   socket.on("chat message", (msg) => {
-//     console.log("message:" + msg);
-//   });
-// });
 
 http.listen(3000, () => {
   console.log("listening on *:3000");
